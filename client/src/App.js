969b6d56239a7df168e2397c041919641ec24873
David Rosenberg <dsr0116@yahoo.com>
2017-10-07 14:51:58 -0400

    made react folder structure

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import NYTContainer from "./components/NYTContainer";
+import NoMatch from "./pages/NoMatch";
+
+//within containg element, include router elements for being able to handle router calls
+const App = () =>
+  <Router>
+    <div>
+      <Switch>
+      	//<Route exact path="/" component={NYTContainer} />
+        //<Route exact path="/saved" component={NYTContainer} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>;
+
+
+export default App;
