efccb670561f7e954cc57b771bc8d5fefe4c60b7
David Rosenberg <dsr0116@yahoo.com>
2017-10-15 18:17:38 -0400

    trying again

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutMS from "./about-ms";
 import AboutSite from "./about-site";
 import Events from "./events"
-import Profile from "./profile"
 import Nav from "../components/nav";
 import Footer from "../components/footer"
 import NoMatch from "./NoMatch";
@@ -18,7 +17,6 @@ const Main = () =>
         		<Route exact path="/" component={AboutMS} />
         		<Route exact path="/about" component={AboutSite} />
         		<Route exact path="/events" component={Events} />
-                <Route exact path="/profile" component={Profile} />
         		<Route component={NoMatch} />
     		</Switch>
             </div>
@@ -27,3 +25,5 @@ const Main = () =>
 	</Router>;
 
 export default Main;
+
+// <Footer />
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,16 +8,6 @@ export default {
   },
   authenticate: function(userData) {
   	return axios.post("/api/auth/authenticate", userData);
-  },
-  uploadImage: function(formData) {
-  	return axios.post("/api/file/upload", formData);
-  },
-  getUser: function(id) {
-    return axios.get("/api/users/" + id);
-  },
-  // Update a profile in the database
-  updateProfile: function(id, profileData) {
-    console.log(profileData);
-    return axios.put("/api/users/" + id, profileData);
   }
+  // Gets all books
 };
