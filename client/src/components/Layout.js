2e6099e8e9c694a275b835d53b86f1e1a8d1bb63
David Rosenberg <dsr0116@yahoo.com>
2017-10-17 20:12:45 -0400

    trying again

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -28,7 +28,7 @@ export default class Layout extends Component {
 	*	Connect to and initializes the socket.
 	*/
 	initSocket = ()=>{
-		const socket = io(socketURL)
+		const socket = io(socketUrl)
 
 		socket.on('connect', ()=>{
 			console.log("connected");
