3a7409613dd5a92e18273935374a1c5c6a955b8a
ngbricen <bricen@gmail.com>
2017-10-19 16:04:41 -0400

    removing warnings

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -4,15 +4,10 @@ import API from "../utils/API";
 import { Modal } from "react-bootstrap";
 import $ from "jquery";
 import { Input, FormBtn } from "../components/Form";
-import { Col, Row, Container } from "../components/Grid";
+import { Col, Row } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import DeleteBtn from "../components/DeleteBtn";
 import SaveBtn from "../components/SaveBtn";
-import Panel from "../components/Panel";
-
-const heading = {
-  "background-color": "#6c99c9"
-}
 
 class Events extends Component {
   // Setting the component's initial state
